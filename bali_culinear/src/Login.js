389f28fee91df5c97bed695b6825821128b04737
fredrick03 <fredlim56@gmail.com>
2023-12-08 23:10:03 +0700

    test https

--- bali_culinear/src/Login.js
+++ bali_culinear/src/Login.js
@@ -37,7 +37,7 @@ const Login = () => {
     const loginToAPI = async () => {
         try {
             const apiUrlLogin1 = 'https://ayokebalitst.azurewebsites.net/signin';
-            const apiUrlLogin2 = 'http://ucanteen2.g3cwh8fvd9frdmeg.southeastasia.azurecontainer.io/login';
+            const apiUrlLogin2 = 'https://ucanteen2.g3cwh8fvd9frdmeg.southeastasia.azurecontainer.io/login';
 
             const dataForApi2 = querystring.stringify({
                 username: username,
